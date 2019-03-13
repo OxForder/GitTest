7c32b0af6bf48f70b972b7e63724eca65ed6de4a
Ford <ford@oxforder.local>
2019-03-13 20:33:52 +0500

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by OxForder on 13/03/2019.
+//  Copyright © 2019 OxForder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
